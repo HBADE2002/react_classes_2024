2505dce1baea28024cbaf74388af80e0b6e16b1f
HBADE2002 <hrishikeshbade007@gmail.com>
2024-03-21 16:04:05 +0530

    Understanding Destructuring using props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Greet = props => {
-    console.log(props);
+const Greet = (props) => {
+    //1. method const Greet = ({name, heroName})
+    //2. method const Greet =(props) , const {name, heroName}=props
+    // Destructuring props in the function parameter to name and heroName
+    // console.log(name); // displays name in the console of the website
+    // console.log(heroName)// {name: "Bruce", heroName: "Batman"}
+    const {name, heroName} = props
     return (
         <div>
-            <h1>Hello, {props.name} a.k.a {props.heroName}</h1>
-            {props.children}
+            <h1>Hello, {name} a.k.a {heroName}</h1>
+            {/* {props.children} */}
         </div>
     )
     
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
 export default class Welcome extends Component {
-  render() {
+  render() { // render method is used to render the JSX to the DOM 
+    const {name, heroName} = this.props // Destructuring props in the class component to name and heroName
+    // const {state1, state2} = this.state // Destructuring state in the class component to state1 and state2
     return (
-        <h1>Welcome to React</h1>
+      <h1>Welcome to React {name} a.k.a {heroName}</h1>  
     )
-  }
+  } // return method is used to return the JSX to the render method
 }
