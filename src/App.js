0651e088cf0941fce063e350c3846c369345af9b
HBADE2002 <hrishikeshbade007@gmail.com>
2024-04-03 12:14:53 +0530

    Made my own additions and added myAge element when button is clicked

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ParentComponent myName='Hrishikesh'/>
+        <ParentComponent myName='Hrishikesh' myAge='21'/>
         {/* <FunctionClick />
         <ClassClick /> */}
       {/* <Counter />
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -5,13 +5,14 @@ class ParentComponent extends Component {
     constructor(props) { // constructor method is called when an object is created from the class and it allows us to initialize the state of the component
         super(props) // super() is used to call the constructor of the parent class and to access the parent's properties and methods
         this.state = {
-            parentName: props.myName // parentName is a state variable that stores the value of the myName prop
+            parentName: props.myName, // parentName is a state variable that stores the value of the myName prop
+            parentAge: props.myAge
         }
         this.greetParent= this.greetParent.bind(this) // binding the method greetParent() to the ParentComponent class
     } 
     
     greetParent() { // greetParent() method is used to alert a message when the button is clicked
-        alert(`Hello ${this.state.parentName}`) // alert() method is used to display an alert box with a specified message and an OK button
+        alert(`Hello ${this.state.parentName} Your age is ${this.state.parentAge}`) // alert() method is used to display an alert box with a specified message and an OK button
     }
   render() { // render() method is used to display the output on the screen
     return (
